f124e7b7e8014eb0529647bb2e8be21e1566e5fd
usuario <bouzambourajae@gmail.comgit config --global user.name raja3-debug>
2025-12-06 23:47:37 +0100

    cambios en .env

--- config/db.php
+++ config/db.php
@@ -3,17 +3,14 @@
 function conexion_bd() {
     require_once __DIR__ . '/vendor/autoload.php'; 
     $env=parse_ini_file(__DIR__. '/../.env');
-    $host=$env['SERVIDOR'];
+    $servidor=$env['SERVIDOR'];
     $user=$env['USER'];
     $bd=$env['DB_BASE'];
-    $bd_pass=$env[''];
-    // $servidor =getenv("SERVIDOR");
-    // $user = getenv("USER");
-    // $password =getenv("DB_PASS");
-    // $bd =getenv("DB_BASE");
+    $bd_pass=$env['DB_PASS'];
+    
     $conn = new mysqli($servidor, $user, $password, $bd);
     if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
+    header("Error de conexión: " . $conn->connect_error);
     }
 
     return $conn;
