db6c6ac71e0cf7f159ad13ad3d10d4596e2211bd
usuario <bouzambourajae@gmail.comgit config --global user.name raja3-debug>
2025-12-07 00:00:01 +0100

    Cambios el funcion de la conexion

--- config/db.php
+++ config/db.php
@@ -2,17 +2,22 @@
 
 function conexion_bd() {
     require_once __DIR__ . '/vendor/autoload.php'; 
-    $env=parse_ini_file(__DIR__. '/../.env');
-    $servidor=$env['SERVIDOR'];
-    $user=$env['USER'];
-    $bd=$env['DB_BASE'];
-    $bd_pass=$env['DB_PASS'];
-    
-    $conn = new mysqli($servidor, $user, $password, $bd);
+
+    $env = parse_ini_file(__DIR__ . '/../.env');
+    $servidor = $env['SERVIDOR'];
+    $user = $env['USER'];
+    $bd = $env['DB_BASE'];
+    $bd_pass = $env['DB_PASS'];
+
+    $conn = new mysqli($servidor, $user, $bd_pass, $bd);
+
     if ($conn->connect_error) {
-    header("Error de conexión: " . $conn->connect_error);
+        // Registrar el error en un archivo log
+        error_log("Error de conexión: " . $conn->connect_error, 3, __DIR__ . "/errores.log");
+        return false;
     }
 
     return $conn;
 }
 ?>
+
