5504fdab78b60a54fc079f8e824ff1b6a2be5610
usuario <bouzambourajae@gmail.comgit config --global user.name raja3-debug>
2025-12-07 00:15:43 +0100

    Cambios en cultivos

--- logic/cultivos.php
+++ logic/cultivos.php
@@ -9,4 +9,24 @@ function cicloCultivo($dias) {
         return "Tardío";
     }
 }
+function ordenarPorDiasAsc($cultivos){
+    usort($cultivos, function($a, $b) {
+        if ($a['dias_cosecha'] < $b['dias_cosecha']) return -1;
+        if ($a['dias_cosecha'] > $b['dias_cosecha']) return 1;
+        return 0;
+    });
+
+    return $cultivos;
+    }
+    function ordenarPorDiasDesc($cultivos){
+        usort($cultivos, function($a, $b) {
+            if ($a['dias_cosecha'] > $b['dias_cosecha']) return -1;
+            if ($a['dias_cosecha'] < $b['dias_cosecha']) return 1;
+            return 0;
+        });
+    
+        return $cultivos;
+        }
+
+
 ?>
